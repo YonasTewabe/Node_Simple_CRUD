9ed0599ed872c32dbd9d97cdb039c9b389794c7a
YonasTewabe <yonastewabe21@gmail.com>
2024-04-05 08:08:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel.js");
+const productRoute = require("./routes/productRoute.js");
+const app = express();
+
+//MIDDLEWARE
+
+app.use(express.json());
+
+//ROUTES
+
+app.use("/api/products", productRoute);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/backenddb")
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("failed to connect");
+  });
